582edd82dc0c0051a6ac95ab590ac6476117ce9b
Spencer Whiteman <spencerwhiteman@Spencers-MacBook-Pro.local>
2022-07-04 15:56:14 -0400

    added info links

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,27 @@
 import Link from "next/link";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import styles from "../styles/Footer.module.css";
+
 
 
 function Footer() {
   return (
-    <footer>
-      <a href="https://github.com/SpencerWhiteman" target='blank'>Github</a>
+    <footer className={styles.footer}>
+
+      <a className={styles.github} href='https://github.com/SpencerWhiteman' rel='noreferrer' target='_blank'>
+        <FontAwesomeIcon icon={faGithub} size='lg' />
+      </a>
+
+      <a href='https://www.instagram.com/spencerwhitemanvisuals/?hl=en' rel='noreferrer' target='_blank'>
+        <FontAwesomeIcon className={styles.instagram} icon={faInstagram} size='lg' />
+      </a>
+
+      <a href='https://www.linkedin.com/in/spencer-whiteman-8a3b7390/' rel='noreferrer' target='_blank'>
+        <FontAwesomeIcon className={styles.linkedin} icon={faLinkedin} size='lg' />
+      </a>
+
+      
     </footer>
   );
 }
